c1749e08269516657609f9c8baf1efa8cef311c6
Idan355 <44299714+Idan355@users.noreply.github.com>
2019-09-07 22:29:25 +0300

    Add files via upload

---
+++ Power/test/PowerTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PowerTest {
+
+    @Test
+    public void power() {
+        assertEquals(4, Power.power(2,2));
+    }
+}
+
+
+
