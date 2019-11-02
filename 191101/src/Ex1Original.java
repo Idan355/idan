893536ff0f157cf439d3130956d4045f5716c56c
Idan355 <44299714+Idan355@users.noreply.github.com>
2019-11-02 20:44:50 +0200

    test

---
+++ 191101/src/Ex1Original.java
@@ -0,0 +1,29 @@
+public class Ex1Original {
+
+
+        class Item {
+            int id;
+            String name;
+        }
+        public class Items{
+            static final int maxinput =10;
+            private int lastchange;
+            private Items[]Items;
+            public Items(){
+                this.Items=new Items[maxinput];
+            }
+        }
+        public void findItem(int id,int index,int Items[]){
+            if(id==Items[index]){
+                System.out.print(index);
+            }
+        }
+
+        public void delete(int index,int Items[]){
+            if (index<10) {
+                Items[index]= 0;
+            }
+        }
+
+
+}
